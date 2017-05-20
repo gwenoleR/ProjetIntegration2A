99edbbe5d1368aee878e6c2196ea9d29e3cc435d
Charles Berdaguer <charles@macbook-pro-de-charles.home>
2017-05-20 16:04:06 +0200

    passage script js a du python pur

--- beacons/beaconNotif.py
+++ beacons/beaconNotif.py
@@ -1,14 +1,43 @@
 from flask import Flask, request, make_response, render_template
 from flask_cors import CORS, cross_origin
+import requests
+import json
+
+url = "http://soc.catala.ovh/v2/translate"
+drapeauxPays = ["🇺🇸", "🇪🇸", "🇫🇷", "🇩🇪", "🇮🇹", "🇧🇷"]
+model_id = ["en-es", "en-fr", "en-de", "en-it", "en-pt"]
+phraseATraduire = "Welcome,you are near the Toto stadium! You can buy your country flag at the stadium's store"
+phraseTraduites = ["", "", "", "", ""]
+
 
 app = Flask(__name__)
 CORS(app)
 
+def getTraduction():
+
+    headers = {
+        "Content-Type":"application/json",
+        "Host": "translator.soc.docker"
+    }
+
+    for i in range(0, 5):
+        
+        data = {
+            "model_id":model_id[i],"source":"fr","target":"es", "text":phraseATraduire
+        }
+
+        r = requests.post(url,data=json.dumps(data),headers=headers)
+        rNoJsoned = json.loads(r.content)
+        print(rNoJsoned)
+        phraseTraduites[i] = rNoJsoned
+
+
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def pageBeacon():
+    return render_template('index.html', listDrapeaux = drapeauxPays, listPhraseTraduites = phraseTraduites, phraseEnglish = phraseATraduire)
 
 
 if __name__ == '__main__':    
     
-    app.run(host='0.0.0.0')
+    getTraduction()
+    app.run(host = "0.0.0.0")
