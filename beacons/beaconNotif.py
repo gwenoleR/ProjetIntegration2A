0a30906e871eb8e0734d4d9cc61a7bfa7cdc18b3
Charles Berdaguer <charles@macbook-pro-de-charles.home>
2017-05-20 12:09:37 +0200

    ajout fichier beacon

---
+++ beacons/beaconNotif.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, make_response, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':    
+    
+    app.run()
