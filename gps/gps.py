dd6b07689f9845eefa4fc9513d71cfcd61c11bb1
Yoann CATALA <yoann@imerir-m14-8.local>
2017-05-21 16:26:38 +0200

    ajout route userInPoi

--- gps/gps.py
+++ gps/gps.py
@@ -28,5 +28,38 @@ def getAllPoi():
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
+@app.route("/userInPoi",methods=['POST'])
+def userInPoi():
+    data = json.loads(request.data)
+    try:
+        u_id = str(data["_id"])
+        poi_id = str(data["poi_id"])
+    except KeyError:
+        resp = make_response("Mauvais formatage du JSON",400)
+        return resp
+    db = initDatabase()
+    userCollection = db.user
+    user_oid = ObjectId(u_id)
+    isUser = userCollection.find_one({"_id":user_oid})
+    if isUser == None:
+        resp = make_response("L'utilisateur n'existe pas !",400)
+        return resp
+    else:
+        poiCollection = db.poi
+        poi_oid = ObjectId(poi_id)
+        isPoi = poiCollection.find_one({"_id":poi_oid})
+        if isPoi == None:
+            resp = make_response("Le poi n'existe pas !",400)
+            return resp
+    isUserInPoi = poiCollection.find_one({"users":u_id})
+    print u_id
+    print(isUserInPoi != None)
+    if isUserInPoi == None:
+        poiCollection.update({'_id': poi_oid}, {'$push': {'users': u_id}})
+        resp = make_response("Modification bien prise en compte",200)
+    else:
+        resp = make_response("L'utilisateur est deja dans la zone !",400)
+    return resp
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
