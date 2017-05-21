a51fc4c17e85214af2af0c9976dad58084f4986b
Yoann CATALA <yoann@imerir-m14-8.local>
2017-05-21 15:19:44 +0200

    requirements pour twitterwall et gps

---
+++ gps/gps.py
@@ -0,0 +1,30 @@
+import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+from flask import Flask, request, make_response,send_file
+
+app = Flask(__name__)
+
+dburl = 'mongodb://soc.catala.ovh:27017/'
+
+def initDatabase():
+    client = MongoClient(dburl)
+    db = client['jo']
+    return db
+
+
+@app.route("/getAllPoi",methods=['GET'])
+def getAllPoi():
+    poiCollection = initDatabase().poi
+    pois = poiCollection.find({})
+    toSend = []
+    for p in pois:
+        p["_id"] = str(p["_id"])
+        toSend.append(p)
+    resp = make_response(json.dumps(toSend),200)
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
